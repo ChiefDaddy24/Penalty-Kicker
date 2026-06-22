8ce4c9de753e05ef548ad590b759e3ce7a98935c
ChiefDaddy24 <jideafolabixx@gmail.com>
2026-06-22 18:46:35 -0500

    fixed classes
    fixed classes

--- pk_class.py
+++ pk_class.py
@@ -1,11 +1,11 @@
 class Player :
-    def __init__(self,name,ability,goals):
-        Player_name = self.name
-        player_ability = self.ability
-        player_goals = self.goals
+    def __init__(self,name,ability):
+         self.player_name = name
+         self.ability = ability
+         self.goals =  0
 
 class goalie:
-    def __init__(self,name,ability,saves):
-        goalie.name = self.name
-        goalie.ability = self.ability
-        goalie.saves = self.saves 
+    def __init__(self,name,ability):
+         self.goalie.name = name 
+         self.goalie_ability = ability
+         self.saves = 0
